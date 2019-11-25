b46a579663573e819a3d09fe49213a72e2ed2a05
Mark Warren <mwarrenus@gmail.com>
2019-11-24 23:03:36 -0800

    Add byte to SI suffix converters

--- bin/human-readable.sh
+++ bin/human-readable.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # https://unix.stackexchange.com/questions/44040/a-standard-tool-to-convert-a-byte-count-into-human-kib-mib-etc-like-du-ls1
-bytesToHuman() {
+human_readable() {
     b=${1:-0}; d=''; s=0; S=(Bytes {K,M,G,T,P,E,Z,Y}iB)
     while ((b > 1024)); do
 	# d="$(printf ".%02d" $((b % 1024 * 100 / 1024)))"
@@ -12,7 +12,7 @@ bytesToHuman() {
 }
 
 if [[ ${#@} -eq 1 ]]; then
-    bytesToHuman $1;
+    human_readable $1;
 else
     echo "Usage: $(basename $0) [bytes]"
 fi
